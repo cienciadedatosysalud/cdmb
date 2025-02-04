1eda94007c3aa61517ebf42c77bb5a018b884e8d
cienciadedatosysalud <129044486+cienciadedatosysalud@users.noreply.github.com>
2025-02-04 16:09:30 +0100

    Delete borrar.py

--- borrar.py
+++
@@ -1,8 +0,0 @@
-from cdmb import launch_ui
-
-server_address = "localhost"
-port = 8001
-
-launch_ui(server_address, port)
-# Open your web browser at http://<server_address>:<port>.
-# http://localhost:8000
